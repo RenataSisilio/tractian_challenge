b627f92571306f0e0f18f35d31b9a26dc99ee2fb
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2024-09-19 18:43:07 -0300

    style: rename file to match class name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import 'core/theme/theme_data.dart';
+import 'core/theme/tractian_theme.dart';
 import 'modules/app_module.dart';
 
 void main() {
